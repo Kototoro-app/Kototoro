3fcd56ca20591932d8b4ca74ef7508200d65f114
skepsun <chuxiongsun@gmail.com>
2026-08-20 15:07:38 +0800

    refactor(ui): drop dead legacy widgets, dialogs, charts and menu providers
    Remove View-system leftovers that have no callers now that the UI is Compose:
    unused core/ui widgets (DotsIndicator, NestedRecyclerView, SegmentedBarView, ...),
    legacy dialog helpers, stats BarChartView/PieChartView, old settings preference
    utilities, dead MenuProvider implementations and leftover RecyclerView selection
    decorations/callbacks. Also delete ContentTypeDialog/SourceTypeDialog which only
    existed to wrap the removed alert-dialog helpers.

--- app/src/main/kotlin/org/skepsun/kototoro/list/ui/adapter/ListHeaderClickListener.kt
+++
@@ -1,9 +0,0 @@
-package org.skepsun.kototoro.list.ui.adapter
-
-import android.view.View
-import org.skepsun.kototoro.list.ui.model.ListHeader
-
-interface ListHeaderClickListener {
-
-	fun onListHeaderClick(item: ListHeader, view: View)
-}
